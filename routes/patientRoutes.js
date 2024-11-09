4743ac18ee54e737ed81120b9b4ac787df81b8ee
rickie <rcya996001@gmail.com>
2024-11-09 11:19:49 -0500

    created API key

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,6 +1,21 @@
 import express from 'express';
 import Patient from '../models/Patient.js';
 
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get current directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env file
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+
+const API_KEY = process.env.API_KEY || "q9328wh3y4tn3ycq89rnyh8oqu4mr98t4w9q8nry0";
+
+
 const router = express.Router();
 
 // Create a new patient
